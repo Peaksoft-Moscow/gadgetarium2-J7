61ea445991bdbe1d59d6f89ebaf84fb775048335
nurte <nurtegingamer@gmail.com>
2024-01-04 14:07:53 +0300

    ProductController done

--- src/main/java/com/peaksoft/gadgetarium2j7/controller/ProductController.java
+++ src/main/java/com/peaksoft/gadgetarium2j7/controller/ProductController.java
@@ -14,6 +14,11 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+
+    @GetMapping("/main-page")
+    public SpecialProductsResponse specialProducts(){
+        return productService.getSpecialProducts();
+    }
     @PostMapping()
     public ResponseEntity<ProductResponse> add(@RequestBody ProductRequest productRequest){
         ProductResponse response = productService.create(productRequest);
