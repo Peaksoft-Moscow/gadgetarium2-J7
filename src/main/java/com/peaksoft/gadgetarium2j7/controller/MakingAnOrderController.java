18bf696e3ef99a508afbbb7b5a6ba4678907506e
nurte <nurtegingamer@gmail.com>
2024-01-16 18:10:57 +0300

    DTO added

---
+++ src/main/java/com/peaksoft/gadgetarium2j7/controller/MakingAnOrderController.java
@@ -0,0 +1,12 @@
+package com.peaksoft.gadgetarium2j7.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/making-an-order")
+@RequiredArgsConstructor
+public class MakingAnOrderController {
+
+}
