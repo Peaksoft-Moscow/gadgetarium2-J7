9b5dae8c8fb4fbeaff269874ee5175ba336c9513
Бекмамат Боронбай уулу <boronbaibekmamat@gmail.com>
2024-01-16 07:20:55 +0300

    Feature/ add Catalog

--- src/main/java/com/peaksoft/gadgetarium2j7/config/SpringSecurity.java
+++ src/main/java/com/peaksoft/gadgetarium2j7/config/SpringSecurity.java
@@ -54,7 +54,7 @@ public class SpringSecurity {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> {
-                    authorize.requestMatchers("/api/users/sign-up","/api/auth/sing-up", "/api/auth/sign-in","/products","/products/search").permitAll()
+                    authorize.requestMatchers("/api/users/sign-up","/api/auth/sing-up", "/api/auth/sign-in","/products","/products/search","/products/search2").permitAll()
                             .anyRequest().authenticated();
                 })
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
