52aeaa8e2704a2a22f0ed22c2eb52a9b83f86a84
nurte <nurtegingamer@gmail.com>
2024-01-16 18:47:06 +0300

    OrderService have a little information

--- src/main/java/com/peaksoft/gadgetarium2j7/service/OrderService.java
+++ src/main/java/com/peaksoft/gadgetarium2j7/service/OrderService.java
@@ -1,4 +1,34 @@
 package com.peaksoft.gadgetarium2j7.service;
 
+import com.peaksoft.gadgetarium2j7.mapper.OrderMapper;
+import com.peaksoft.gadgetarium2j7.model.dto.OrderRequest;
+import com.peaksoft.gadgetarium2j7.model.dto.OrderResponse;
+import com.peaksoft.gadgetarium2j7.model.entities.Order;
+import com.peaksoft.gadgetarium2j7.repository.OrderRepository;
+import com.peaksoft.gadgetarium2j7.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class OrderService {
+    private final OrderMapper orderMapper;
+    private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+
+    public OrderResponse create(OrderRequest request){
+            Order order = orderMapper.mapToEntity(request);
+            orderRepository.save(order);
+            return orderMapper.mapToResponse(order);
+        }
+        public List<OrderResponse> getAll() {
+            List<OrderResponse> orderResponses = new ArrayList<>();
+            for (Order order : orderRepository.findAll()){
+                orderResponses.add(orderMapper.mapToResponse(order));
+            }
+            return orderResponses;
+        }
 }
