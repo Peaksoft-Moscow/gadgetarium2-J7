e876994e6362e50510b558280629b46775bc983f
bekkg <b.kgkg98@gmail.com>
2024-01-18 19:52:54 +0300

    compare product

--- src/main/java/com/peaksoft/gadgetarium2j7/repository/CategoryRepository.java
+++ src/main/java/com/peaksoft/gadgetarium2j7/repository/CategoryRepository.java
@@ -1,6 +1,5 @@
 package com.peaksoft.gadgetarium2j7.repository;
 
-import com.peaksoft.gadgetarium2j7.model.entities.Brand;
 import com.peaksoft.gadgetarium2j7.model.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
