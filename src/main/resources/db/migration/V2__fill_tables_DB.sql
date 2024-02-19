1cc7bf43b87db2c87ad0c1c4d432e5c2da51adf2
NurtikGitler <nurtegingamer@gmail.com>
2024-02-19 16:58:46 +0300

    checkpoint

--- src/main/resources/db/migration/V2__fill_tables_DB.sql
+++ src/main/resources/db/migration/V2__fill_tables_DB.sql
@@ -12,7 +12,9 @@ insert into users (age, create_date,
                    name,
                    password,
                    tel_number) VALUES
-                                   (18, now(), 1, 1, 1,
-                                    'Pushkin street, Kolotushkin house, num 5',
-                                    'email@gmail.com',
-                                    'name', 'UNISEX', 'lastname', 'name','password', '+7 999 999 99 99')
+                                   (18, now(), null, 1, null,
+                                    'Unknown street, num 5',
+                                    'admin@gmail.com',
+                                    'name', 'UNISEX', 'lastname', 'Admin','password', '+7 999 999 99 99'),
+                                   (18,now(),1,2,1,'Unknown street, num 12','user@gmail.com','name','UNISEX','lastname','User','password','+7 900 900 90 90');
+insert into users_and_roles (role_id, user_id) VALUES (1,1), (2,1);
